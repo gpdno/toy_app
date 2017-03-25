1f70cc9cd8ef8dbea8ae9bc404dc9a3501f0e796
gpdno <gpdno@mac.com>
2017-03-25 15:34:53 -0400

    updated styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   
   root to: 'static_pages#home'
 
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
 
   resources :microposts
   resources :users
