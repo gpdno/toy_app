6f39e58161a87aabf26abbc622dac61f05529a7c
gpdno <gpdno@mac.com>
2017-03-25 14:25:01 -0400

    added bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #helper to return full title on page even if one is not given
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
